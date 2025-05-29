b5e9b55001953d0ecfd88ce480fbaf458ce42e2c
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-05-29 17:04:01 +0100

    modified

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,25 +1,94 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const startButton = document.querySelector(".button");
-const input = document.querySelector("#datetime-picker");
-
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const startButton = document.querySelector(".button");
+const input = document.querySelector("#datetime-picker");
+
+const daysEl = document.querySelector("[data-days]");
+const hoursEl = document.querySelector("[data-hours]");
+const minutesEl = document.querySelector("[data-minutes]");
+const secondsEl = document.querySelector("[data-seconds]");
+
+let userSelectedDate = null;
+let timerId = null;
+
+// Спочатку кнопка Start неактивна
 startButton.disabled = true;
 
-
-
-const options = {
+flatpickr(input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+    const selectedDate = selectedDates[0];
+    const now = new Date();
 
-if()
+    if (selectedDate <= now) {
+      userSelectedDate = null;
+      startButton.disabled = true;
+
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+        position: "topRight",
+      });
+
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateTimerUI({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function startCountdown() {
+  timerId = setInterval(() => {
+    const now = new Date();
+    const timeLeft = userSelectedDate - now;
+
+    if (timeLeft <= 0) {
+      clearInterval(timerId);
+      updateTimerUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      input.disabled = false;
+      startButton.disabled = true;
+      return;
+    }
+
+    const time = convertMs(timeLeft);
+    updateTimerUI(time);
+  }, 1000);
+}
+
+startButton.addEventListener("click", () => {
+  startButton.disabled = true;
+  input.disabled = true;
+  startCountdown();
+});
